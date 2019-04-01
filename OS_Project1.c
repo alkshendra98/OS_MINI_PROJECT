ffbe148c378581eed52fff8345e86ecd1d63448c
Alkshendra Kumar <akgangwar989@gmail.com>
2019-04-01 15:13:09 +0530

    Adding 2nd Functiona In project

--- OS_Project1.c
+++ OS_Project1.c
@@ -23,6 +23,8 @@ void fcfs(int Process[],int n,int Burst_time[])
 	findwt(Process,n,Burst_time,wt);
 	findtat(Process,n,Burst_time,wt,tat);
 	int i;
+	printf("\n\n");
+	printf("-------------First come first serve---------------\n");
 	printf("Process Burst Time Waiting time TurnArroundTime\n");
 	for(i=0;i<n;i++)
 	{
@@ -40,6 +42,58 @@ void fcfs(int Process[],int n,int Burst_time[])
     printf("\nAverage Turn Around Time is : %d ",b);
 	
 }
+void sjf(int Process[],int Burst_time[], int n)
+{
+	int wt[n],tat[n],i,j,temp,total=0,pos;
+	float avg_wt,avg_tat;
+	 //sorting burst time in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(Burst_time[j]<Burst_time[pos])
+                pos=j;
+        }
+ 
+        temp=Burst_time[i];
+        Burst_time[i]=Burst_time[pos];
+        Burst_time[pos]=temp;
+ 
+        temp=Process[i];
+        Process[i]=Process[pos];
+        Process[pos]=temp;
+    }
+ 
+    wt[0]=0;            //waiting time for first process will be zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=Burst_time[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=(float)total/n;      //average waiting time
+    total=0;
+    printf("\n\n");
+    printf("-------------------Shortest Job First-------------------------");
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=Burst_time[i]+wt[i];     //calculate turnaround time
+        total+=tat[i];
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",Process[i],Burst_time[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=(float)total/n;     //average turnaround time
+    printf("\n\nAverage Waiting Time=%f",avg_wt);
+    printf("\nAverage Turnaround Time=%f\n",avg_tat);
+	
+}
 
 int main()
 {
@@ -47,4 +101,5 @@ int main()
    int Burst_time[]={8,4,1};
    int n=sizeof Process/sizeof Process[0];
    fcfs(Process,n,Burst_time);
+   sjf(Process,Burst_time,n);
 }
